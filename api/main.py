2da436ca23433d5ef5eadd495bcbf8506583c11d
NjayyousiN <nasseraljayyousi@gmail.com>
2025-05-22 12:59:08 +0300

    Allow CORS

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from v1.courses.route import router as course_router
 from v1.auth.route import router as auth_router
 
@@ -8,6 +9,13 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(course_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 @app.get("/")  
 def read_root():
